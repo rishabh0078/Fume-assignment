cf039e7c5eaa29e1359a09c2d8e2cc683cebd626
rishabh <rishabhchaudhary058@gmail.com>
2026-07-21 10:02:07 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ app = FastAPI(title="GenAI Client Intelligence Dashboard API")
 # Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=False,
+    allow_origin_regex=r".*",
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
